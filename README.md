5bcb5b565477533eccc926bb512d4276878bb083
Carlos Valdivia <vpcarlos97@gmail.com>
2021-06-18 23:39:38 +0200

    Fix apt argument missing

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 CLI to help users deploy Odoo on with docker and an nginx network.
 
-
+sudo apt install python3-pip
 ### Developing Odan
 ```bash
 $ pip install -e . [dev]
--- odan/tools/env.py
+++ odan/tools/env.py
@@ -2,27 +2,30 @@ import os
 import json
 import secrets
 import string
+import subprocess
 from contextlib import contextmanager
-from odan.tools.tools import run_command
 
 
-def apt(command, packages=[]):
-    run_command(["sudo", "apt-get", "-y", command] + packages)
+def apt(command, packages=[], stream_output=False):
+    run_command(
+        ["sudo", "apt-get", "-y", command] + packages,
+        stream_output=stream_output
+    )
 
 
-def apt_update():
-    apt("update")
+def apt_update(stream_output=False):
+    apt("update", stream_output=stream_output)
 
 
-def apt_install(packages):
-    apt("install", packages)
+def apt_install(packages, stream_output=False):
+    apt("install", packages, stream_output=stream_output)
 
 
-def apt_remove(packages):
-    apt("remove", packages)
+def apt_remove(packages, stream_output=False):
+    apt("remove", packages, stream_output=stream_output)
 
 
-def check(package):
+def check(package, stream_output=False):
     return run_command([package, "--version"], raise_error=False)
 
 
@@ -32,10 +35,6 @@ def rm(file):
 
 def check_dir(directory):
     return os.path.isdir(directory)
-
-
-def create_dir(directory):
-    return os.mkdir(directory)
 
 
 @contextmanager
@@ -49,3 +48,31 @@ def cwd(new_cwd=None):
         yield os.chdir(new_cwd)
     finally:
         os.chdir(cwd)
+
+
+def run_command(command, stream_output=False, raise_error=True):
+    """
+    Run a Linux command.
+    Here, check_output is used to retrieve the command output when
+    an exception is raised.
+    """
+    try:
+        if stream_output:
+            subprocess.check_call(
+                command, stderr=subprocess.STDOUT)
+        else:
+            subprocess.check_output(
+                command, stderr=subprocess.STDOUT)
+        return True
+    except subprocess.CalledProcessError as e:
+        error_msg = "The '%s' command has failed" % e.cmd[0]
+        if not stream_output:
+            error_msg += " with the following output:\n %s" % e.output.decode(
+                "utf-8"
+            ).rstrip("\n")
+        if raise_error:
+            raise Warning(error_msg)
+    except FileNotFoundError as error_msg:
+        if raise_error:
+            raise Warning(error_msg)
+        return False
